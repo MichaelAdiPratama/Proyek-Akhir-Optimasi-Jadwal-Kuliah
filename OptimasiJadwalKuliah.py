69cae8afacf3bd5330476ed60eb1594cf5d7aea8
MichaelAdiPratama <c14210016@john.petra.ac.id>
2023-06-06 14:50:45 +0700

    fungsi tambah jadwal

--- OptimasiJadwalKuliah.py
+++ OptimasiJadwalKuliah.py
@@ -1,5 +1,23 @@
+# import library
 import random
 import pandas as pd
 from tkinter import *
 from tkinter import messagebox
 import datetime as dt
+
+# Inisiasi array Jadwal Kuliah
+jadwal_kuliah = []
+
+# fungsi add jadwal kuliah untuk menambahkan jadwal kuliah:
+def tambah_jadwal():
+    jam_mulai = int(jam_mulai_entry.get())
+    jam_selesai = int(jam_selesai_entry.get())
+    nama_dosen = nama_dosen_entry.get()
+    mata_kuliah = mata_kuliah_entry.get()
+    sks = int (sks_entry.get())
+    ruangan = ruangan_entry.get()
+    hari = hari_var.get()
+    jadwal_kuliah.append([hari,mata_kuliah,nama_dosen,jam_mulai, jam_selesai, ruangan,sks])
+    jadwal_df = pd.DataFrame(jadwal_kuliah,columns=["Jam Mulai", "Jam Selesai", "Nama Dosen", "Mata Kuliah", "SKS", "Ruangan", "Hari"])
+    jadwal_text.delete(1.0,END)
+    jadwal_text.insert(END,jadwal_df.to_string(index=False))
